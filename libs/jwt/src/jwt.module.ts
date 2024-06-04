4ad35ef78d46a577b07e204828e7c9811835836d
Ilya Ovchinnikov <christopher.wo0d@iclud.com>
2024-06-04 16:06:30 +0500

    upd: jwt module

--- libs/jwt/src/jwt.module.ts
+++ libs/jwt/src/jwt.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from './jwt.service';
-
+import { JwtModule as Jwt } from '@nestjs/jwt';
 @Module({
+	imports: [Jwt.register({})],
 	providers: [JwtService],
 	exports: [JwtService],
 })
--- libs/jwt/src/jwt.service.ts
+++ libs/jwt/src/jwt.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService as JWTService } from '@nestjs/jwt';
 
+import { IJwtPayload } from 'libs/typings/jwt/payload';
 @Injectable()
-export class JwtService {}
+export class JwtService {
+	constructor(private readonly jwtService: JWTService) {}
+
+	/**
+	 * Генерация пары токенов
+	 * @param payload
+	 * @returns
+	 */
+	public async generate(payload: IJwtPayload) {
+		const [accessToken, refreshToken] = await Promise.all([
+			this.jwtService.signAsync(
+				{
+					...payload,
+				},
+				{
+					secret: process.env.JWT_ACCESS_SECRET,
+					expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+				},
+			),
+			this.jwtService.signAsync(
+				{
+					...payload,
+				},
+				{
+					secret: process.env.JWT_REFRESH_SECRET,
+					expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+				},
+			),
+		]);
+
+		return {
+			accessToken,
+			refreshToken,
+		};
+	}
+
+	/**
+	 * Проверка валидности токена
+	 * @param type - тип токена (access/refresh)
+	 * @param token - токен
+	 * @returns Promise<IJwtPayload | null>
+	 */
+	public async vaildate(
+		type: 'access' | 'refresh',
+		token: string,
+	): Promise<IJwtPayload | null> {
+		const secret =
+			type === 'access'
+				? process.env.JWT_ACCESS_SECRET
+				: process.env.JWT_REFRESH_SECRET;
+
+		try {
+			const payload = <IJwtPayload>await this.jwtService.verifyAsync(
+				token,
+				{
+					secret,
+				},
+			);
+			return payload || null;
+		} catch (error) {
+			return null;
+		}
+	}
+}

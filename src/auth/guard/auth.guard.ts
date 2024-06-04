e6785b278e8ab131e9b9f46ef7b6cec9a4630f69
Ilya Ovchinnikov <christopher.wo0d@iclud.com>
2024-06-04 16:06:07 +0500

    add: auth guard

---
+++ src/auth/guard/auth.guard.ts
@@ -0,0 +1,37 @@
+import { JwtService } from '@common/jwt';
+import {
+	CanActivate,
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common';
+import { IRequest } from 'libs/typings/core/irequest';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+	constructor(private readonly jwtService: JwtService) {}
+
+	async canActivate(context: ExecutionContext) {
+		const req = <IRequest>context.switchToHttp().getRequest();
+
+		const token = req.headers.authorization;
+
+		if (!token) {
+			throw new UnauthorizedException('Пользователь не автризован!');
+		}
+
+		const [bearer, jwtToken] = token.split(' ');
+
+		if (bearer !== 'Bearer') {
+			throw new UnauthorizedException('Пользователь не автризован!');
+		}
+
+		const user = await this.jwtService.vaildate('access', jwtToken);
+		if (user === null) {
+			throw new UnauthorizedException('Пользователь не автризован!');
+		}
+
+		req.user = user;
+		return true;
+	}
+}
